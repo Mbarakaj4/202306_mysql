d2a62f0bd156f94a27c27287a36a19443b877e9f
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-08-09 15:32:26 -0400

    Update sqlcountries

--- Core/CodingDojo-MySQLCountries/sqlcountries.sql
+++ Core/CodingDojo-MySQLCountries/sqlcountries.sql
@@ -16,8 +16,28 @@ ON `countries`.`id` = `languages`.`country_id`
 WHERE `languages`.`percentage` > 89
 ORDER BY `languages`.`percentage` DESC;
 
-SELECT `countries`.`name`, `countries`.`government_form`, `countries`.`life_expectancy`
+SELECT `countries`.`name`, `countries`.`government_form`, `countries`.`life_expectancy`, `countries`.`capital`
 FROM `countries`
 WHERE `countries`.`government_form` = 'Constitutional Monarchy' 
-AND `countries`.`life_expectancy` > 75;
+AND `countries`.`life_expectancy` > 75 AND `countries`.`capital` > 200;
 
+SELECT `name`, `surface_area`, `population`
+FROM `countries`
+WHERE `surface_area` < 501 AND `population` > 100000;
+
+SELECT `countries`.`name` AS `country_name`, `cities`.`name` AS `city_name`, 
+`cities`.`district`, `cities`.`population`
+FROM `countries` JOIN `cities`
+ON `countries`.`id` = `cities`.`country_id`
+WHERE `cities`.`population` > 500000 AND `cities`.`district` = 'Buenos Aires';
+
+SELECT `countries`.`region`, COUNT(`countries`.`name`) AS `countries`
+FROM `countries`
+GROUP BY `region`
+ORDER BY `countries` DESC;
+
+SELECT `countries`.`name`, COUNT(`cities`.`name`) AS `cities`
+FROM `countries` JOIN `cities`
+ON `countries`.`id` = `cities`.`country_id`
+GROUP BY `countries`.`name`
+ORDER BY `cities` DESC;
